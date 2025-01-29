199b28380850d1277dfd08afa189ca287e656c51
Chris <143294640+Chris2Rich@users.noreply.github.com>
2025-01-29 13:17:25 +0000

    !

--- lib/sha/sha256.cpp
+++ lib/sha/sha256.cpp
@@ -1,18 +1,9 @@
 #include <stdint.h>
+#include <iostream>
 #include <vector>
 #include <bitset>
-#include <algorithm>
-
-uint32_t K[64] {
-  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-  0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-  0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-  0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-  0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-  0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-};
+#include <string>
+#include <sstream>
 
 // convert string to binary string
 std::vector<bool> stobin(std::vector<unsigned char> inp){
@@ -33,7 +24,18 @@ constexpr inline uint32_t rotr(const uint32_t x, int n) {
   return (x >> n) | (x << (-n & mask));
 }
 
-std::vector<unsigned char> sha256(std::vector<unsigned char> inp) {
+uint32_t K[64] {
+  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+  0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+  0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+  0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+  0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+  0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+std::string sha256(std::vector<unsigned char> inp) {
   uint32_t h0 = 0x6a09e667;
   uint32_t h1 = 0xbb67ae85;
   uint32_t h2 = 0x3c6ef372;
@@ -116,25 +118,24 @@ std::vector<unsigned char> sha256(std::vector<unsigned char> inp) {
       a = temp1 + temp2;
     }
 
-    h0 = h0 + a;
-    h1 = h1 + b;
-    h2 = h2 + c;
-    h3 = h3 + d;
-    h4 = h4 + e;
-    h5 = h5 + f;
-    h6 = h6 + g;
-    h7 = h7 + h;
+    h0 += a;
+    h1 += b;
+    h2 += c;
+    h3 += d;
+    h4 += e;
+    h5 += f;
+    h6 += g;
+    h7 += h;
   }
 
-  std::vector<unsigned char> digest;
-  uint32_t hashes[] {h0, h1, h2, h3, h4, h5, h6, h7};
+  std::stringstream digest;
+  digest << std::hex << h0 << h1 << h2 << h3 << h4 << h5 << h6 << h7;
+  return digest.str();
+}
 
-  for (uint32_t hash : hashes) {
-      digest.push_back((hash >> 24) & 0xFF);
-      digest.push_back((hash >> 16) & 0xFF);
-      digest.push_back((hash >> 8) & 0xFF);
-      digest.push_back(hash & 0xFF);
-  }
-
-  return digest;
+int main(){
+    std::vector<unsigned char> a = {};
+    std::string res = sha256(a);
+    std::cout << res;
+    return 0;
 }
