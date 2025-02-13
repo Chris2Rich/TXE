684b981c488d94b802e1d41ba86b5b6708947711
Chris <143294640+Chris2Rich@users.noreply.github.com>
2025-02-13 12:13:58 +0000

    !

--- src/blockchain/block.cpp
+++ src/blockchain/block.cpp
@@ -1,18 +1,17 @@
-#include <string>
 #include <stdint.h>
-#include <bitset>
-#include "/src/tx/tx.cpp"
+#include "./src/tx/tx.cpp"
 
 struct header {
-    std::string id; //hash of previous block, uses sha512 so 512 bit output, once verified the block's tx data can be requested from validator
-    std::string nonce; // for nonce space, initialized to 128 bits
-    std::string merkel_root;
-    std::bitset<512> difficulty; //proposed difficulty, verified by consensu
+    unsigned char version; // if version changes, this allows for backwards compatability
+    unsigned char id[64]; //hash of previous block, uses sha512 so 512 bit output, once verified the block's tx data can be requested from validator, as the model of the ledger is NOT a blockchain, multiple blocks can point to a single ancestor, therefore they will need to have different nonces, regular model of blockchain consensus with MCW (most cumulative work) will be used to determine which branch has precedence IF they use the same nonce, otherwise there is limited potential for future id collision.
+    unsigned char nonce[64]; // for nonce space, this should be equal to the size of the domain of the hash function
+    unsigned char merkel_root[64];
+    uint32_t difficulty; //proposed difficulty, verified by consensus
 };
 
 struct block {
-    std::string id; //stored in db and ID is used for rapid indexing
-    void* tx_list;
+    unsigned char id[64]; //stored in db and ID is used for rapid indexing into b-tree
+    tx tx_list[];
 };
 
 int main(){
--- src/tx/tx.cpp
+++ src/tx/tx.cpp
@@ -1,3 +1,3 @@
 struct tx{
-    
+    unsigned char txid[8];
 };
