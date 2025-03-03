ee6d036150fa670ace238d8c1c88a65f4275437e
Chris2Rich <AzkronTheGamer@gmail.com>
2025-03-03 21:50:45 +0000

    ?

--- src/blockchain/block.cpp
+++ src/blockchain/block.cpp
@@ -4,11 +4,12 @@
 #include <cstring>
 #include "../../cryptopp/cryptlib.h"
 #include "../../cryptopp/sha.h"
+#include <vector>
 
 struct header {
-    unsigned char version; // if version changes, this allows for backwards compatability
-    unsigned char prevhash[64]; //hash of concatenation of ancestors' data - allows for unique ids with ancestors
-    unsigned char ancestors[256]; //concatenation of the ids of the 4 ancestors a block can have. if only 1 true ancestor then use genesis block. 
+    unsigned int version; // if version changes, this allows for backwards compatability
+    unsigned char id[64]; //hash of concatenation of ancestors' data - allows for unique ids with ancestors
+    std::vector<unsigned char> ancestors; //concatenation of the ids of the ancestors a block has. 
     unsigned char nonce[64]; // for nonce space, this should be equal to the size of the domain of the hash function
     unsigned char merkel_root[64];
     uint32_t difficulty; //proposed difficulty, verified by consensus
@@ -16,53 +17,77 @@ struct header {
 
 struct block {
     unsigned char id[64]; //stored in db and ID is used for rapid indexing into b-tree
-    unsigned int tx_count;
-    tx tx_list[];
+    std::vector<tx> tx_list;
 
     //returns sha512 of the merkle root of the block(more random)
-    void create_merkle_root(unsigned char* res){
+    void create_merkle_root(unsigned char* res) {
         CryptoPP::SHA512 hash;
-        unsigned char digest[64];
+        std::vector<std::vector<unsigned char>> nodes;
+        std::vector<unsigned char> digest(64);
+        unsigned char zero_hash[64] = {0};
 
-        unsigned const char* nodes[(unsigned int)pow(2, ceil(log2(tx_count)))]; // array of pointers to strings
-        for(unsigned int i = 0; i < pow(2, ceil(log2(tx_count))); i++){
-            if(i < tx_count){
-                hash.Update(tx_list[i].id, strlen(reinterpret_cast<const char*>(tx_list[i].id)));
-                hash.Final(digest);
-                nodes[i] = digest;
+        size_t tx_count = tx_list.size();
+        if (tx_count == 0) {
+            memset(res, 0, 64);
+            return;
+        }
+
+        // Compute next power of 2
+        size_t leaf_count = 1;
+        while (leaf_count < tx_count) {
+            leaf_count <<= 1;
+        }
+
+        // Create leaf nodes
+        for (size_t i = 0; i < leaf_count; i++) {
+            if (i < tx_count) {
+                hash.Update(tx_list[i].id, 64);
             } else {
-                hash.Update((const unsigned char*)"0", 1);
-                hash.Final(digest);
-                nodes[i] = digest;
+                hash.Update(zero_hash, 64);
             }
+            hash.Final(digest.data());
+            nodes.push_back(digest);
         }
-        unsigned const char** prev = nodes;
-        unsigned int height = (unsigned int)(ceil(log2(tx_count))) - 1;
-        while(height >= 0){
-            unsigned const char* level[(unsigned int)pow(2, (unsigned int)(ceil(log2(tx_count))) - 1)];
-            for(unsigned int i = 0; i < pow(2, (unsigned int)(ceil(log2(tx_count))) - 2); i++){
-                unsigned char concat[128];
-                memcpy(concat, prev[i*2], 512);
-                memcpy(concat + 64, prev[(i*2) + 1], 512);
-                hash.Update(concat, 1024);
-                hash.Final(digest);
-                level[i] = digest;
-            }
-            prev = level;
-            height -= 1;
 
-            if(height == 0){
-                hash.Update(level[0], 1024);
-                hash.Final(digest);
-                res = digest;
-                return;
+        // Build Merkle tree
+        while (nodes.size() > 1) {
+            std::vector<std::vector<unsigned char>> new_nodes;
+            for (size_t i = 0; i < nodes.size(); i += 2) {
+                std::vector<unsigned char> concat(128, 0);
+                memcpy(concat.data(), nodes[i].data(), 64);
+
+                if (i + 1 < nodes.size()) {
+                    memcpy(concat.data() + 64, nodes[i + 1].data(), 64);
+                } else {
+                    // Hash the last node with itself if odd number
+                    hash.Update(nodes[i].data(), 64);
+                    hash.Update(nodes[i].data(), 64);
+                    hash.Final(digest.data());
+                    new_nodes.push_back(digest);
+                    break;
+                }
+
+                hash.Update(concat.data(), 128);
+                hash.Final(digest.data());
+                new_nodes.push_back(digest);
             }
+
+            nodes = new_nodes;
         }
-        res = nullptr;
-        return;
-    };
+
+        // Copy the Merkle root
+        memcpy(res, nodes[0].data(), 64);
+    }
 };
 
 int main(){
+    block bloc;
+    tx a(61);
+    tx b(62);
+    tx c(63);
+    tx d(64);
+    unsigned char res[64];
+    bloc.tx_list = {a,b,c,d};
+    bloc.create_merkle_root(res);
     return 0;
 }
--- src/tx/tx.cpp
+++ src/tx/tx.cpp
@@ -3,25 +3,28 @@
 #include <cstring>
 #include "../../cryptopp/cryptlib.h"
 #include "../../cryptopp/sha.h"
+#include <vector>
 
 struct tx_in{
-    unsigned char blob[256];
+    unsigned char blob[256]; //whatever data is needed to unlock the coin
 };
 
 struct tx_out{
     uint64_t amount;
-    unsigned char blob[256];
+    unsigned char blob[256]; //the code that will be ran to unlock the coin in the future
 };
 
 struct tx{
     unsigned char version;
     unsigned char id[64]; //generated from double hash of the data it contains
     
-    tx_in* inputs;
-    unsigned char input_n;
+    std::vector<tx_in> inputs;
+    std::vector<tx_out> outputs;
 
-    tx_out* outputs;
-    unsigned char output_n;
+    tx(unsigned char i){
+        unsigned char v[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,i};
+        memcpy(id, v, 64);
+    }
 
     const unsigned char* create_tx_id(){
         const unsigned char* res = (const unsigned char*)"Test"; 
