2b2fb6a8eaf12c60201d5d21325232f31d13ec36
Chris <143294640+Chris2Rich@users.noreply.github.com>
2025-03-25 15:34:41 +0000

    it works???

--- TXE-core/src/main.cpp
+++ TXE-core/src/main.cpp
@@ -1,5 +1,16 @@
 #include "/workspaces/ecc/TXE-core/include/tx.h"
 #include "/workspaces/ecc/TXE-core/include/block.h"
+#include <iostream>
+#include <iomanip>
+
+std::string charArrayToHexString(const unsigned char* arr, size_t length) {
+    std::ostringstream oss;
+    oss << std::hex << std::setfill('0');
+    for (size_t i = 0; i < length; ++i) {
+        oss << std::setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(arr[i]));
+    }
+    return oss.str();
+}
 
 int main(){
     block bloc;
@@ -10,5 +21,6 @@ int main(){
     unsigned char res[64];
     bloc.tx_list = {a,b,c,d};
     bloc.create_merkle_root(res);
+    std::cout << charArrayToHexString(res, 32);
     return 0;
 }
